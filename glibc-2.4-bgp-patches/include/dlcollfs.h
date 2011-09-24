e8d5e9ca17519ba027e050407044b669a4868c87
Aron Ahmadia <aron@ahmadia.net>
2011-09-24 17:46:54 +0300

    some more work towards exposing ld.so functions through libc

--- glibc-2.4-bgp-patches/include/dlcollfs.h
+++ glibc-2.4-bgp-patches/include/dlcollfs.h
@@ -6,14 +6,14 @@
 
 /* Now define the internal interfaces.  */
 
-extern int __libc_dlcollfs_comm_push (MPI_Comm comm);
+extern int __libc_dlcollfs_comm_push (MPI_Comm);
 extern void __libc_dlcollfs_init_pointers ( int (*_MPI_Initialized)(int *flag),
-                                             int (*_MPI_Comm_rank) (MPI_Comm comm, int *rank),
-                                             int (*_MPI_Bcast) (void *buffer, int count, 
-                                                                MPI_Datatype datatype, int root,MPI_Comm comm),
-                                             int (*_MPI_Allreduce) (void *sendbuf, void *recvbuf, int count,
-                                                                    MPI_Datatype datatype, MPI_Op op,
-                                                                    MPI_Comm comm),
+                                            int (*_MPI_Comm_rank) (MPI_Comm comm, int *rank),
+                                            int (*_MPI_Bcast) (void *buffer, int count, 
+                                                               MPI_Datatype datatype, int root,MPI_Comm comm),
+                                            int (*_MPI_Allreduce) (void *sendbuf, void *recvbuf, int count,
+                                                                   MPI_Datatype datatype, MPI_Op op,
+                                                                   MPI_Comm comm),
                                             int (*_MPI_Barrier) (MPI_Comm comm));
   
 libc_hidden_proto (__libc_dlcollfs_comm_push);
@@ -34,14 +34,14 @@ struct dlcollfs_hook
 {
   int (*dlcollfs_comm_push) (MPI_Comm comm);
   void (*dlcollfs_init_pointers) ( int (*_MPI_Initialized)(int *flag),
-                                 int (*_MPI_Comm_rank) (MPI_Comm comm, int *rank),
-                                 int (*_MPI_Bcast) (void *buffer, int count, 
-                                                    MPI_Datatype datatype, int root,MPI_Comm comm),
-                                 int (*_MPI_Allreduce) (void *sendbuf, void *recvbuf, int count,
-                                                        MPI_Datatype datatype, MPI_Op op,
-                                                        MPI_Comm comm),
-                                 int (*_MPI_Barrier) (MPI_Comm comm))
-  void *pad[4];
+                                   int (*_MPI_Comm_rank) (MPI_Comm comm, int *rank),
+                                   int (*_MPI_Bcast) (void *buffer, int count, 
+                                                      MPI_Datatype datatype, int root,MPI_Comm comm),
+                                   int (*_MPI_Allreduce) (void *sendbuf, void *recvbuf, int count,
+                                                          MPI_Datatype datatype, MPI_Op op,
+                                                          MPI_Comm comm),
+                                   int (*_MPI_Barrier) (MPI_Comm comm),
+                                   void *pad[4]);
 };
 
 extern struct dlcollfs_hook *_dlcollfs_hook;
